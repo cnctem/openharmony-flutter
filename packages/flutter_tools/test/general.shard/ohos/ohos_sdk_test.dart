1af7a92820e476ec56c2f9f71ff9bb61f2480093
qiao_jianqiang <qiaojianqiang2@h-partners.com>
2024-10-26 17:19:25 +0800

    add unit test

---
+++ packages/flutter_tools/test/general.shard/ohos/ohos_sdk_test.dart
@@ -0,0 +1,173 @@
+/*
+* Copyright (c) 2024 Hunan OpenValley Digital Industry Development Co., Ltd.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import 'package:file/memory.dart';
+import 'package:flutter_tools/src/base/config.dart';
+import 'package:flutter_tools/src/base/file_system.dart';
+import 'package:flutter_tools/src/globals.dart' as globals;
+import 'package:flutter_tools/src/ohos/ohos_sdk.dart';
+import 'package:json5/json5.dart';
+
+import '../../src/common.dart';
+import '../../src/context.dart';
+
+void main() {
+  late MemoryFileSystem fileSystem;
+  late Config config;
+
+  setUp(() {
+    fileSystem = MemoryFileSystem.test();
+    config = Config.test();
+  });
+
+  group('HmosSdk', () {
+    testUsingContext('parse hmos sdk', () {
+      final Directory sdkDir = createHmosSdkDirectory(fileSystem: fileSystem);
+      config.setValue('ohos-sdk', sdkDir.path);
+      final HmosSdk sdk = HmosSdk.localHmosSdk()!;
+
+      expect(sdk.apiAvailable, isNotNull);
+      expect(sdk.apiAvailable, <String>['12:default']);
+    }, overrides: <Type, Generator>{
+      FileSystem: () => fileSystem,
+      ProcessManager: () => FakeProcessManager.any(),
+      Config: () => config,
+    });
+
+    testUsingContext('get hdc path', () {
+      final Directory sdkDir = createHmosSdkDirectory(fileSystem: fileSystem);
+      config.setValue('ohos-sdk', sdkDir.path);
+      final HmosSdk sdk = HmosSdk.localHmosSdk()!;
+
+      expect(sdk.hdcPath, isNotNull);
+    }, overrides: <Type, Generator>{
+      FileSystem: () => fileSystem,
+      ProcessManager: () => FakeProcessManager.any(),
+      Config: () => config,
+    });
+
+    testUsingContext('valid directory', () {
+      final Directory sdkDir = createHmosSdkDirectory(fileSystem: fileSystem);
+      config.setValue('ohos-sdk', sdkDir.path);
+      final HmosSdk sdk = HmosSdk.localHmosSdk()!;
+
+      expect(sdk.isValidDirectory, isTrue);
+    }, overrides: <Type, Generator>{
+      FileSystem: () => fileSystem,
+      ProcessManager: () => FakeProcessManager.any(),
+      Config: () => config,
+    });
+  });
+
+  group('OhosSdk', () {
+    testUsingContext('parse ohos sdk', () {
+      final Directory sdkDir = createSdkDirectory(fileSystem: fileSystem);
+      config.setValue('ohos-sdk', sdkDir.path);
+      final OhosSdk sdk = OhosSdk.localOhosSdk()!;
+
+      expect(sdk.apiAvailable, isNotNull);
+      expect(sdk.apiAvailable, <String>['12:12', '10:10']);
+    }, overrides: <Type, Generator>{
+      FileSystem: () => fileSystem,
+      ProcessManager: () => FakeProcessManager.any(),
+      Config: () => config,
+    });
+
+    testUsingContext('get hdc path', () {
+      final Directory sdkDir = createSdkDirectory(fileSystem: fileSystem);
+      config.setValue('ohos-sdk', sdkDir.path);
+      final OhosSdk sdk = OhosSdk.localOhosSdk()!;
+
+      expect(sdk.hdcPath, isNotNull);
+    }, overrides: <Type, Generator>{
+      FileSystem: () => fileSystem,
+      ProcessManager: () => FakeProcessManager.any(),
+      Config: () => config,
+    });
+
+    testUsingContext('valid directory', () {
+      final Directory sdkDir = createSdkDirectory(fileSystem: fileSystem);
+      config.setValue('ohos-sdk', sdkDir.path);
+      final OhosSdk sdk = OhosSdk.localOhosSdk()!;
+
+      expect(sdk.isValidDirectory, isTrue);
+    }, overrides: <Type, Generator>{
+      FileSystem: () => fileSystem,
+      ProcessManager: () => FakeProcessManager.any(),
+      Config: () => config,
+    });
+  });
+
+  test('isNumeric', () {
+    expect(HarmonySdk.isNumeric('12'), isTrue);
+  });
+}
+
+void _createSdkFile(Directory dir, String filePath, {String? contents}) {
+  final File file = dir.childFile(filePath);
+  file.createSync(recursive: true);
+  if (contents != null) {
+    file.writeAsStringSync(contents, flush: true);
+  }
+}
+
+Directory createHmosSdkDirectory({
+  required FileSystem fileSystem,
+}) {
+  final Directory dir =
+      fileSystem.systemTempDirectory.createTempSync('flutter_mock_ohos_sdk.');
+  final String exe = globals.platform.isWindows ? '.exe' : '';
+
+  void createDir(Directory dir, String path) {
+    final Directory directory =
+        dir.fileSystem.directory(dir.fileSystem.path.join(dir.path, path));
+    directory.createSync(recursive: true);
+  }
+
+  createDir(dir, 'default/hms');
+  final Map<String, dynamic> sdkPkg = <String, dynamic>{};
+  final Map<String, dynamic> dataObj = <String, dynamic>{};
+  dataObj['version'] = '5.0.0.68';
+  dataObj['apiVersion'] = '12';
+  sdkPkg['data'] = dataObj;
+  final String jsonData = JSON5.stringify(sdkPkg);
+  _createSdkFile(dir, 'default/sdk-pkg.json', contents: jsonData);
+  _createSdkFile(dir, 'default/openharmony/toolchains/hdc$exe');
+  return dir;
+}
+
+Directory createSdkDirectory({
+  bool withLicenses = false,
+  required FileSystem fileSystem,
+}) {
+  final Directory dir =
+      fileSystem.systemTempDirectory.createTempSync('flutter_mock_ohos_sdk.');
+  final String exe = globals.platform.isWindows ? '.exe' : '';
+
+  void createDir(Directory dir, String path) {
+    final Directory directory =
+        dir.fileSystem.directory(dir.fileSystem.path.join(dir.path, path));
+    directory.createSync(recursive: true);
+  }
+
+  if (withLicenses) {
+    createDir(dir, 'licenses');
+  }
+  createDir(dir, '10');
+  createDir(dir, '12');
+  _createSdkFile(dir, '10/toolchains/hdc$exe');
+  _createSdkFile(dir, '12/toolchains/hdc$exe');
+  return dir;
+}
---
+++ packages/flutter_tools/test/general.shard/ohos/ohos_workflow_test.dart
@@ -0,0 +1,112 @@
+/*
+* Copyright (c) 2024 Hunan OpenValley Digital Industry Development Co., Ltd.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import 'package:flutter_tools/src/ohos/ohos_sdk.dart';
+import 'package:flutter_tools/src/ohos/ohos_workflow.dart';
+import 'package:test/fake.dart';
+
+import '../../src/common.dart';
+import '../../src/fakes.dart';
+
+void main() {
+  testWithoutContext('OhosWorkflow handles a null OhosSDK', () {
+    final OhosWorkflow ohosWorkflow = OhosWorkflow(
+      featureFlags: TestFeatureFlags(),
+      ohosSdk: null,
+    );
+
+    expect(ohosWorkflow.canLaunchDevices, false);
+    expect(ohosWorkflow.canListDevices, false);
+    expect(ohosWorkflow.canListEmulators, false);
+  });
+
+  testWithoutContext('OhosWorkflow handles a null hdc', () {
+    final FakeOhosSdk ohosSdk = FakeOhosSdk();
+    ohosSdk.hdcPath = null;
+    final OhosWorkflow ohosWorkflow = OhosWorkflow(
+      featureFlags: TestFeatureFlags(),
+      ohosSdk: ohosSdk,
+    );
+
+    expect(ohosWorkflow.canLaunchDevices, false);
+    expect(ohosWorkflow.canListDevices, false);
+    expect(ohosWorkflow.canListEmulators, false);
+  });
+
+  testWithoutContext('Support for Ohos SDK on Linux Arm Hosts', () {
+    final FakeOhosSdk ohosSdk = FakeOhosSdk();
+    ohosSdk.hdcPath = null;
+    final OhosWorkflow ohosWorkflow = OhosWorkflow(
+      featureFlags: TestFeatureFlags(isOhosEnabled: true),
+      ohosSdk: ohosSdk,
+    );
+
+    expect(ohosWorkflow.appliesToHostPlatform, isTrue);
+    expect(ohosWorkflow.canLaunchDevices, isFalse);
+    expect(ohosWorkflow.canListDevices, isFalse);
+    expect(ohosWorkflow.canListEmulators, isFalse);
+  });
+
+  testWithoutContext('OhosWorkflow is disabled if feature is disabled', () {
+    final FakeOhosSdk ohosSdk = FakeOhosSdk();
+    ohosSdk.hdcPath = null;
+    final OhosWorkflow ohosWorkflow = OhosWorkflow(
+      featureFlags: TestFeatureFlags(isOhosEnabled: false),
+      ohosSdk: ohosSdk,
+    );
+
+    expect(ohosWorkflow.appliesToHostPlatform, false);
+    expect(ohosWorkflow.canLaunchDevices, false);
+    expect(ohosWorkflow.canListDevices, false);
+    expect(ohosWorkflow.canListEmulators, false);
+  });
+
+  testWithoutContext('OhosWorkflow cannot list emulators if hdcPath is null',
+      () {
+    final FakeOhosSdk ohosSdk = FakeOhosSdk();
+    ohosSdk.hdcPath = null;
+    final OhosWorkflow ohosWorkflow = OhosWorkflow(
+      featureFlags: TestFeatureFlags(isOhosEnabled: true),
+      ohosSdk: ohosSdk,
+    );
+
+    expect(ohosWorkflow.appliesToHostPlatform, true);
+    expect(ohosWorkflow.canLaunchDevices, false);
+    expect(ohosWorkflow.canListDevices, false);
+    expect(ohosWorkflow.canListEmulators, false);
+  });
+
+  testWithoutContext('OhosWorkflow can list emulators', () {
+    final FakeOhosSdk ohosSdk = FakeOhosSdk();
+    ohosSdk.hdcPath = 'path/to/hdc';
+    final OhosWorkflow ohosWorkflow = OhosWorkflow(
+      featureFlags: TestFeatureFlags(isOhosEnabled: true),
+      ohosSdk: ohosSdk,
+    );
+
+    expect(ohosWorkflow.appliesToHostPlatform, true);
+    expect(ohosWorkflow.canLaunchDevices, true);
+    expect(ohosWorkflow.canListDevices, true);
+    expect(ohosWorkflow.canListEmulators, true);
+  });
+}
+
+class FakeOhosSdk extends Fake implements OhosSdk {
+  @override
+  String sdkPath = '';
+
+  @override
+  String? hdcPath;
+}
