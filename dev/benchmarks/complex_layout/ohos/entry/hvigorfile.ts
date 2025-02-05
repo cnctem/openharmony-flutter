447768b206f7d6a8105c0b940a3317b24e389eba
chenzhensheng79 <15118029047@139.com>
2025-02-05 17:22:23 +0800

    update license info
    Signed-off-by: chenzhensheng79 <15118029047@139.com>

--- dev/benchmarks/complex_layout/ohos/entry/hvigorfile.ts
+++ dev/benchmarks/complex_layout/ohos/entry/hvigorfile.ts
@@ -1,8 +1,4 @@
-/*
-* Copyright (c) 2023 Hunan OpenValley Digital Industry Development Co., Ltd. All rights reserved.
-* Use of this source code is governed by a BSD-style license that can be
-* found in the LICENSE_KHZG file.
-*/
+
 
 // Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
 export { hapTasks } from '@ohos/hvigor-ohos-plugin';
